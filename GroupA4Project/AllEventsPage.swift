6882dba1a2e3619a8fe488b2dabf216bcf6895ad
Hillary <hillarytrinh8@gmail.com>
2023-07-20 13:01:06 -0400

    Uploaded AllEventsPage

--- GroupA4Project/AllEventsPage.swift
+++ GroupA4Project/AllEventsPage.swift
@@ -9,7 +9,78 @@ import SwiftUI
 
 struct AllEventsPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("All Astronomical Events")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                VStack{
+                    
+                    NavigationLink(destination: EventDescriptionPage()) {
+                        GroupBox("Mercury at Greatest Elongation East")
+                        {
+                            HStack {
+                                Image("0")
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                VStack{
+                                    Text("Southern Hemisphere: West 08/09/23: Shortly after sunset")
+                                        .lineSpacing(6.0)
+                                }
+                            }
+                        }
+                        .padding()
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                    }
+                    
+                    
+                    NavigationLink(destination: EventDescriptionPage()) {
+                        GroupBox("Perseid Meteors")
+                        {
+                            HStack {
+                                Image("1")
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                VStack{
+                                    Text("Northern Hemisphere: Northeast        08/13/23: Midnight")
+                                        .lineSpacing(/*@START_MENU_TOKEN@*/6.0/*@END_MENU_TOKEN@*/)
+                                }
+                            }
+                        }
+                        .padding(.horizontal)
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                    }
+                    
+                    NavigationLink(destination: EventDescriptionPage()) {
+                        GroupBox("Saturn at Opposition")
+                        {
+                            HStack {
+                                Image("2")
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                VStack{
+                                    Text("East                                       08/26/23: Overnight")
+                                        .lineSpacing(/*@START_MENU_TOKEN@*/6.0/*@END_MENU_TOKEN@*/)
+                                }
+                            }
+                        }
+                        .padding()
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                    }
+                }
+            }
+        }
     }
 }
 
